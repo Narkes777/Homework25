57bf543d4bb767419e54d2cd72949175a7f47a7a
Narkes777 <62197668+Narkes777@users.noreply.github.com>
2023-10-18 15:05:15 +0600

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,57 @@
+# #Task 1
+# Age = int(input("Введите ваш возраст: "))
+# print(Age)
+
+# #Task 2
+# Friends_list = ["Marik", "Sekran", "Simple", "God", "Bot"]
+# for friend in Friends_list:
+#     print(friend)
+
+# #Task 3
+# age = int(input("Введите ваш возраст: "))
+
+# if age >= 18:
+#     print("Вы совершеннолетний")
+# else:
+#     print("Вы несовершеннолетний")
+
+# #Task 4
+# Friends_list = ["Marik", "Sekran", "Simple"]
+
+# for name in Friends_list:
+#     print(f"Привет, {name}!")
+
+# #Task 5
+# age = 18 
+
+# while age < 30:
+#     print(f"Возраст: {age}")
+#     age += 1
+
+# #Task 6
+# def add_numbers(num1, num2):
+#     result = num1 + num2
+#     return result
+
+# number1 = 5
+# number2 = 7
+# sum_result = add_numbers(number1, number2)
+
+# print(f"Сумма {number1} и {number2} равна {sum_result}")
+
+# #Task 7
+# def print_greeting(name):
+#     print(f"Привет, {name}!")
+
+# your_name = "Sekran"
+# print_greeting(your_name)
+
+# #Task 8
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+
+# result = factorial(5)
+# print(f"Факториал числа 5 равен {result}")
